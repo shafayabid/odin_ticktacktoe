156d5742dbc2d1a0bafa69f96acf3ee9c8402f44
Shafay Abid <shafayabid4@gmail.com>
2024-09-10 12:48:13 +0500

    complete flow

--- script.js
+++ script.js
@@ -31,16 +31,17 @@ function Gameboard() {
         // so we need to loop through the rows, starting at row 0,
         // find all the rows that don't have a token, then take the
         // last one, which will represent the bottom-most empty cell
-        const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
+        // const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
 
         // If no cells make it through the filter, 
         // the move is invalid. Stop execution.
-        if (!availableCells.length) return;
-
-        console.log(`row is ${row} and column is ${column}`)
+        // if (!availableCells.length) return;
 
         // Otherwise, I have a valid cell, the last one in the filtered array
-        board[row][column].addToken(player);
+        const currentCell = board[row][column]
+        if (currentCell.getValue() === "") {
+            board[row][column].addToken(player);
+        }
     };
 
     // This method will be used to print our board to the console.
@@ -64,7 +65,7 @@ function Gameboard() {
 */
 
 function Cell() {
-    let value = 0;
+    let value = "";
 
     // Accept a player's token to change the value of the cell
     const addToken = (player) => {
@@ -94,15 +95,16 @@ function GameController(
     const players = [
         {
             name: playerOneName,
-            token: 1
+            token: 'X'
         },
         {
             name: playerTwoName,
-            token: 2
+            token: 'O'
         }
     ];
 
     let activePlayer = players[0];
+    let gameWon = false;
 
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -114,19 +116,65 @@ function GameController(
         console.log(`${getActivePlayer().name}'s turn.`);
     };
 
-    const playRound = (column, row) => {
+    const playRound = (column, row, playerWon) => {
         // Drop a token for the current player
-        console.log(
-            `Dropping ${getActivePlayer().name}'s token into column ${column}...`
-        );
-        board.dropToken(column, row, getActivePlayer().token);
+        if (!gameWon) {
+            console.log(
+                `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+            );
+            board.dropToken(column, row, getActivePlayer().token);
+        }
 
         /*  This is where we would check for a winner and handle that logic,
             such as a win message. */
 
         // Switch player turn
-        switchPlayerTurn();
-        printNewRound();
+        if (checkWinner()) {
+            gameWon = true;
+            console.log(`${getActivePlayer().name} has won the game!`);
+            playerWon.textContent = `${getActivePlayer().name} has won!!...`
+        } else {
+            switchPlayerTurn();
+            printNewRound();
+        }
+    };
+
+    const checkWinner = () => {
+        const currentBoard = board.getBoard();
+
+        // Check rows
+        for (let i = 0; i < 3; i++) {
+            if (currentBoard[i][0].getValue() !== '' &&
+                currentBoard[i][0].getValue() === currentBoard[i][1].getValue() &&
+                currentBoard[i][1].getValue() === currentBoard[i][2].getValue()) {
+                return true; // Row win
+            }
+        }
+
+        // Check columns
+        for (let i = 0; i < 3; i++) {
+            if (currentBoard[0][i].getValue() !== '' &&
+                currentBoard[0][i].getValue() === currentBoard[1][i].getValue() &&
+                currentBoard[1][i].getValue() === currentBoard[2][i].getValue()) {
+                return true; // Column win
+            }
+        }
+
+        // Check main diagonal
+        if (currentBoard[0][0].getValue() !== '' &&
+            currentBoard[0][0].getValue() === currentBoard[1][1].getValue() &&
+            currentBoard[1][1].getValue() === currentBoard[2][2].getValue()) {
+            return true; // Main diagonal win
+        }
+
+        // Check anti-diagonal
+        if (currentBoard[0][2].getValue() !== '' &&
+            currentBoard[0][2].getValue() === currentBoard[1][1].getValue() &&
+            currentBoard[1][1].getValue() === currentBoard[2][0].getValue()) {
+            return true; // Anti-diagonal win
+        }
+
+        return false; // No winner yet
     };
 
     // Initial play game message
@@ -144,53 +192,52 @@ function GameController(
 function ScreenController() {
     const game = GameController();
     const playerTurnDiv = document.querySelector('.turn');
+    const playerWonDiv = document.querySelector('.won');
     const boardDiv = document.querySelector('.board');
-  
+
     const updateScreen = () => {
-      // clear the board
-      boardDiv.textContent = "";
-  
-      // get the newest version of the board and player turn
-      const board = game.getBoard();
-      const activePlayer = game.getActivePlayer();
-  
-      // Display player's turn
-      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
-  
-      // Render board squares
-      board.forEach((row, rowIndex) => {
-        row.forEach((cell, colIndex) => {
-          // Anything clickable should be a button!!
-          const cellButton = document.createElement("button");
-          cellButton.classList.add("cell");
-          // Create a data attribute to identify the column
-          // This makes it easier to pass into our `playRound` function 
-          cellButton.dataset.column = colIndex
-          cellButton.dataset.row = rowIndex
-          cellButton.textContent = cell.getValue();
-          boardDiv.appendChild(cellButton);
+        // clear the board
+        boardDiv.textContent = "";
+
+        // get the newest version of the board and player turn
+        const board = game.getBoard();
+        const activePlayer = game.getActivePlayer();
+
+        // Display player's turn
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+        // Render board squares
+        board.forEach((row, rowIndex) => {
+            row.forEach((cell, colIndex) => {
+                // Anything clickable should be a button!!
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                // Create a data attribute to identify the column
+                // This makes it easier to pass into our `playRound` function 
+                cellButton.dataset.column = colIndex
+                cellButton.dataset.row = rowIndex
+                cellButton.textContent = cell.getValue();
+                boardDiv.appendChild(cellButton);
+            })
         })
-      })
     }
-  
+
     // Add event listener for the board
     function clickHandlerBoard(e) {
-      const selectedColumn = e.target.dataset.column;
-      const selectedRow = e.target.dataset.row;
-      // Make sure I've clicked a column and not the gaps in between
-      if (!selectedColumn) return;
-      
-        console.log(`row 2: ${selectedRow}`)
+        const selectedColumn = e.target.dataset.column;
+        const selectedRow = e.target.dataset.row;
+        // Make sure I've clicked a column and not the gaps in between
+        if (!selectedColumn) return;
 
-      game.playRound(selectedColumn, selectedRow);
-      updateScreen();
+        game.playRound(selectedColumn, selectedRow, playerWonDiv);
+        updateScreen();
     }
     boardDiv.addEventListener("click", clickHandlerBoard);
-  
+
     // Initial render
     updateScreen();
-  
+
     // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
-  }
-  
-  ScreenController();
+}
+
+ScreenController();
